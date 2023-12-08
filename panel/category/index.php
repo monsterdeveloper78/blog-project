7b84746f95fd957ae19cc0e4d73a354e571bb21d
Amir Kashir <monsterdeveloper78@gmail.com>
2023-12-08 19:02:49 +0330

    delete section

--- panel/category/index.php
+++ panel/category/index.php
@@ -53,7 +53,7 @@ require_once '../../functions/pdo_connection.php';
                                 <td><?= $category->name ?></td>
                                 <td>
                                     <a href="" class="btn btn-info btn-sm">Edit</a>
-                                    <a href="" class="btn btn-danger btn-sm">Delete</a>
+                                    <a href="<?= url('panel/category/delete.php?cat_id='). $category->id  ?>" class="btn btn-danger btn-sm">Delete</a>
                                 </td>
                             </tr>
 
